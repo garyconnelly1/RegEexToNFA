02a9ed1bd2da8026cc1a95c65d5ba5747cb1e7cf
ZqhKHP <G00336837@gmit.ie>
2018-03-28 15:36:26 +0100

    adding comments to the for range of infix loop

--- Shunting.go
+++ Shunting.go
@@ -16,23 +16,31 @@ func intopost(infix string)string{
 //for each character in the expression
 	for _, r := range infix{
 		switch {
-			
+			//if the character = '(', simply append that character onto the array s.
 		case r == '(' :
 			s = append(s,r)
 
+			//if the character = ')', while the last index in the s array, append the last index of the s array to the pofix array, 
+			//and clear the s array up to the value in the last index of that array.
 		case r == ')' :
 			for(s[len(s)-1] != '('){
 				pofix, s = append(pofix, s[len(s)-1]), s[:len(s)-1]  
 			}//end for
+			//equate the array s to just the last index in that array
 			s = s[:len(s)-1]  
 
+			//if the character = one of the desegnated characters in the specials map,
 		case specials[r] > 0 :
-			//for
+			//for when the lenght of array s in greater than 0 and specials array at index r(the current character),
+			//is less than or = to the value at the specials array at index of the value at the last index of array s,
 			for(len(s) > 0 && specials[r] <= specials[s[len(s)-1]] ){
+				//append the last index of array s to pofix, and clear array s up to the last index
 				pofix, s = append(pofix, s[len(s)-1]), s[:len(s)-1] 
 			}//end for
+			//append r(current character to array s.)
 			s = append(s,r)
 		default:
+			//if the current character does not meet one of the above cases, append the current character to pofix array.
 		pofix = append(pofix,r)
 		}
 	
