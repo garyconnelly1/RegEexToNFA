07431e8e9ac87876c3aa547fe57b2e9191fd8e63
ZqhKHP <G00336837@gmit.ie>
2018-03-28 15:15:04 +0100

    adding main function that outputs the infix notation

--- Shunting.go
+++ Shunting.go
@@ -5,3 +5,27 @@ package main
 import (
 	"fmt"
 )
+
+//function for changing infix to post fix notation
+func intopost(infix string)string{
+	return infix
+}
+
+//main function
+func main(){
+	// postfix = ab.c*.
+	fmt.Println("Infix: ", "a.b.c*")
+	fmt.Println("Postfix: ", intopost("a.b.c*"))
+
+	// postfix = abd|.*
+	fmt.Println("Infix: ", "(a.(b|d))*")
+	fmt.Println("Postfix: ", intopost("(a.(b|d))*"))
+	
+	// postfix = abb.+.c.
+	fmt.Println("Infix: ", "a.(b.b)+.c")
+	fmt.Println("Postfix: ", intopost("a.(b.b)+.c"))
+
+	// postfix = abb.+.c.
+	fmt.Println("Infix: ", "n.a.m.e")
+	fmt.Println("Postfix: ", intopost("n.a.m.e"))
+}
