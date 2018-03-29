219d90ea5157a533a40e1d109090afa304809510
ZqhKHP <G00336837@gmit.ie>
2018-03-29 15:12:26 +0100

    converting the postfix regex to an nfa

--- Thompsons.go
+++ Thompsons.go
@@ -17,3 +17,53 @@ type nfa struct{
 	initial *state
 	accept *state
 }
+
+func poregtonfa(pofix string) *nfa{
+	nfastack := []*nfa{}
+
+	for _, r := range pofix {
+		switch r{
+		case '.':
+			frag2 := nfastack[len(nfastack)-1]
+			nfastack = nfastack[:len(nfastack)-1]
+			frag1 := nfastack[len(nfastack)-1]
+			nfastack = nfastack[:len(nfastack)-1]
+
+			frag1.accept.edge1 = frag2.initial
+
+			nfastack = append(nfastack, &nfa{initial: frag1.initial, accept: frag2.accept})
+		case '|':
+			frag2 := nfastack[len(nfastack)-1]
+			nfastack = nfastack[:len(nfastack)-1]
+			frag1 := nfastack[len(nfastack)-1]
+			nfastack = nfastack[:len(nfastack)-1]
+
+			//new states
+			initial := state{edge1: frag1.initial, edge2: frag2.initial}
+			accept := state{}
+			frag1.accept.edge1 = &accept
+			frag2.accept.edge1 = &accept
+
+
+			nfastack = append(nfastack, &nfa{initial: &initial, accept: &accept})
+		case '*':
+			frag := nfastack[len(nfastack)-1]
+			nfastack = nfastack[:len(nfastack)-1]
+
+			accept := state{}
+			initial := state{edge1: frag.initial, edge2: &accept}
+			frag.accept.edge1 = frag.initial
+			frag.accept.edge2 = &accept
+
+			nfastack = append(nfastack, &nfa{initial: &initial, accept: &accept})
+		default:
+			accept := state{}
+			initial := state{symbol: r, edge1: &accept}
+
+			nfastack = append(nfastack, &nfa{initial: &initial, accept: &accept})
+
+		}//end switch
+	}//end for range
+
+	return nfastack[0]
+}//end function
